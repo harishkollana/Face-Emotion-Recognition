fdb8d0edfe0a60d9efe20ac2e1f8e9e28ea44cbb
Harish K <59809982+harishkollana@users.noreply.github.com>
2022-03-18 18:40:15 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -50,10 +50,10 @@ class Faceemotion(VideoTransformerBase):
           cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)  
 
           resized_img = cv2.resize(test_img, (1000, 700))  
-          b=cv2.imshow('Facial emotion analysis ',resized_img)  
+          cv2.imshow('Facial emotion analysis ',resized_img)  
 
-      return b
-
+          
+          
 def main():
   # Face Analysis Application #
   st.title("Live Class Monitoring System")
